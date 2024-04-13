9d7ac251684f1e84318368eba0ed64599d160084
ginlink <ginlink1998@gmail.com>
2024-04-13 11:47:02 +0800

    init

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/shuding/nextra-docs-template",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Nextra Docs Template",
   },
-}
+  i18n: [
+    { locale: "en-US", text: "English" },
+    { locale: "zh-CN", text: "中文" },
+  ],
+};
 
-export default config
+export default config;
